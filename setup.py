2ca52c6f50bc68ab35dd5891536ae297ba33651b
bart holmberg <bart.holmberg@gmail.com>
2023-02-07 16:15:51 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
+# BAH, TODO: pybind11 repo 
 from typing import List
 import atexit
 import glob
